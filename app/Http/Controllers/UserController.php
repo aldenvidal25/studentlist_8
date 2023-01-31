83bf251e8d18a93eb11f4b064cafa1890d46a3c8
Alden Vidal <aldenvidal25@gmail.com>
2023-01-31 14:24:36 +0800

    initial commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -11,12 +11,14 @@ use Illuminate\Validation\Rule;
 
 class UserController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return 'Hello from UserController';
     }
 
-    public function login() {
-    if(FacadesView::exists('user.login')){
+    public function login()
+    {
+        if (FacadesView::exists('user.login')) {
             return view('user.login');
         } else {
             return abort(404);
@@ -24,25 +26,28 @@ class UserController extends Controller
         }
     }
 
-    public function process (Request $request){
+    public function process(Request $request)
+    {
         $validated = $request->validate([
             "email" => ['required', 'email'],
             "password" => 'required'
-       ]);
+        ]);
 
-       if(auth()->attempt($validated)){
+        if (auth()->attempt($validated)) {
             $request->session()->regenerate();
 
             return redirect('/')->with('message', 'Welcome back!');
-       }       
-       return back()->withErrors(['email' => 'Login failed'])->onlyInput('email');
+        }
+        return back()->withErrors(['email' => 'Login failed'])->onlyInput('email');
     }
 
-    public function register() {
+    public function register()
+    {
         return view('user.register');
     }
 
-    public function logout(Request $request){
+    public function logout(Request $request)
+    {
         auth()->logout();
 
         $request->session()->invalidate();
@@ -51,28 +56,30 @@ class UserController extends Controller
         return redirect('/login')->with('message', 'Logout successfull');
     }
 
-    public function store (Request $request){
-       $validated = $request->validate([
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
             "name" => ['required', 'min:4'],
-            "email" => ['required', 'email', Rule::unique('users','email')],
+            "email" => ['required', 'email', Rule::unique('users', 'email')],
             "password" => 'required|confirmed|min:6',
-       ]);
+        ]);
 
-       $validated['password'] = Hash::make($validated['password']);
+        $validated['password'] = Hash::make($validated['password']);
 
-       $user = User::create($validated);
-
-       auth()->login($user);
-        
+        $user = User::create($validated);
+        auth()->login($user);
+        return redirect('/')->with('message', 'Account created successfully!');
+        // auth()->login($user);
     }
 
-    public function show($id) {
+    public function show($id)
+    {
         $data = ["data" => "data from database"];
         return view('user')
-                ->with('data', $data)
-                ->with('name', 'Kodego SP404 Class')
-                ->with('age', '19')
-                ->with('email', 'sp404@kodego.com.ph')
-                ->with('id', $id);
+            ->with('data', $data)
+            ->with('name', 'Kodego SP404 Class')
+            ->with('age', '19')
+            ->with('email', 'sp404@kodego.com.ph')
+            ->with('id', $id);
     }
 }
